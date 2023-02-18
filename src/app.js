bd6f8de7a6e80a23c13b86625c369bb4ee9497ba
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-02-18 16:46:15 +0530

    updating hbs

--- src/app.js
+++ src/app.js
@@ -3,17 +3,23 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const hbs = require('hbs');
+
 require('./db/conn');
 
 const port = process.env.PORT || 5000;
 
-//connect to html page
-
 const static_path = path.join(__dirname, '../public');
+const template_path = path.join(__dirname, '../templates/views');
+const partials_path = path.join(__dirname, '../templates/partials');
+
 app.use(express.static(static_path));
+app.set('view engine', 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path);
 
 app.get('/', (req, res) => {
-	res.send('hello Myself Shivanshu Pathak');
+	res.render('index');
 });
 
 app.listen(port, () => {
