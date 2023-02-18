ed7d9c87530f7df6385c207fc4307742f0139f3e
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-02-18 16:10:08 +0530

    connected updating page

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 /** @format */
 
 const express = require('express');
+const path = require('path');
 const app = express();
 require('./db/conn');
 
 const port = process.env.PORT || 5000;
 
+//connect to html page
+
+const static_path = path.join(__dirname, '../public');
+app.use(express.static(static_path));
+
 app.get('/', (req, res) => {
-	res.send('hello from thapa technical');
+	res.send('hello Myself Shivanshu Pathak');
 });
 
 app.listen(port, () => {
